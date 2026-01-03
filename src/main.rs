267d6d237d3e1d200a388e042852c9caf7dce4da
Zephirus2 <andycostovici@gmail.com>
2026-01-03 19:33:49 +0100

    New launch options.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{Parser, ValueEnum};
 use serde::Deserialize;
 use std::{
     fs,
@@ -12,18 +13,18 @@ use std::{
 const PIXEL_THRESHOLD: i32 = 3;
 
 // The distance from the top at which the bar will hide again.
-
 const PIXEL_THRESHOLD_SECONDARY: i32 = 50;
 const MOUSE_REFRESH_DELAY_MS: u64 = 100;
 
 fn main() {
+    let args = Args::parse();
     let (tx, rx) = mpsc::channel::<Event>();
 
     let mut cursor_top: bool = false;
     let mut windows_opened: bool = check_windows();
     let mut last_visibility: bool = !windows_opened;
 
-    spawn_mouse_position_updated(tx.clone());
+    spawn_mouse_position_updater(tx.clone(), args.clone());
     spawn_window_event_listener(tx.clone());
 
     tx.send(Event::CursorTop(false)).ok();
@@ -38,7 +39,16 @@ fn main() {
             Event::WindowsOpened(val) => windows_opened = val,
         }
 
-        let current_visible = if cursor_top { true } else { !windows_opened };
+        let current_visible = match args.always_hidden {
+            true => cursor_top,
+            false => {
+                if cursor_top {
+                    true
+                } else {
+                    !windows_opened
+                }
+            }
+        };
 
         if current_visible != last_visibility {
             // Refreshes PID if it was lost or not found yet
@@ -61,7 +71,7 @@ fn main() {
 }
 
 /// Keeps track of the mouse position
-fn spawn_mouse_position_updated(tx: Sender<Event>) {
+fn spawn_mouse_position_updater(tx: Sender<Event>, args: Args) {
     thread::spawn(move || {
         let mut previous_state = false;
         loop {
@@ -75,13 +85,19 @@ fn spawn_mouse_position_updated(tx: Sender<Event>) {
                 });
 
                 if let Some(m) = active_monitor {
-                    let local_y = pos.y - m.y;
+                    let distance_from_edge = match args.side {
+                        Side::Top => pos.y - m.y,
+                        Side::Bottom => (m.y + m.height) - pos.y,
+                        Side::Left => pos.x - m.x,
+                        Side::Right => (m.x + m.width) - pos.x,
+                    };
+
                     let threshold = if previous_state {
                         PIXEL_THRESHOLD_SECONDARY
                     } else {
                         PIXEL_THRESHOLD
                     };
-                    let is_cursor_top = local_y <= threshold;
+                    let is_cursor_top = distance_from_edge <= threshold;
 
                     if is_cursor_top != previous_state {
                         tx.send(Event::CursorTop(is_cursor_top)).ok();
@@ -186,4 +202,28 @@ struct Monitor {
     y: i32,
     width: i32,
     height: i32,
+}
+
+#[derive(Parser, Clone)]
+struct Args {
+    /// If set, the bar will always hide when the cursor is not at the top
+    #[arg(long)]
+    always_hidden: bool,
+    /// Which side of the screen the bar is located on. Beware that doesn't work well with multiple monitors.
+    #[arg(long, value_enum, default_value = "top")]
+    side: Side,
+}
+
+#[derive(Debug, Clone, Copy, Deserialize, ValueEnum)]
+enum Side {
+    Top,
+    Left,
+    Right,
+    Bottom,
+}
+
+impl Default for Side {
+    fn default() -> Self {
+        Side::Top
+    }
 }
